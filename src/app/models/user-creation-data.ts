00c8b666cb7dbfd6600748e04467032caaedc882
georgrybski <rybskigeorg@gmail.com>
2023-02-25 01:08:35 -0300

    ~ Adicionando modelos de usuário

---
+++ src/app/models/user-creation-data.ts
@@ -0,0 +1,7 @@
+export interface UserCreationData {
+  name: string;
+  login: string;
+  password: string;
+  email: string;
+  birthDate: Date;
+}
---
+++ src/app/models/user-view-data.ts
@@ -0,0 +1,9 @@
+export interface UserViewData {
+  id: number;
+  name: string;
+  login: string;
+  email: string;
+  birthDate: Date;
+  creationTime?: Date;
+  updateTime?: Date;
+}

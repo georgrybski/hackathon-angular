a73d66c8633053a961c12dff8838677b05ab7d25
georgrybski <rybskigeorg@gmail.com>
2023-02-28 01:12:27 -0300

    ~ Adicionando UserCrudTableComponent

---
+++ src/app/components/user-crud-table/user-crud-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-crud-table',
+  templateUrl: './user-crud-table.component.html',
+  styleUrls: ['./user-crud-table.component.css']
+})
+export class UserCrudTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

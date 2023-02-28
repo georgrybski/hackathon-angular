1b272baa667b9fd8229daa2eddb1c99bfca0fd07
georgrybski <rybskigeorg@gmail.com>
2023-02-28 01:02:04 -0300

    ~ Adicionadno imports, providers e declarações previstas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { UserCrudTableComponent } from './components/user-crud-table/user-crud-table.component';
+import { UserService } from './services/user.service';
+import { FileUploadModule } from 'primeng/fileupload';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ValidatorService} from './services/validator-service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCrudTableComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MenuModule,
+    MenubarModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TableModule,
+    FileUploadModule,
+    FormsModule,
+    DialogModule,
+    ConfirmDialogModule,
+    InputNumberModule,
+    InputTextModule,
+    RadioButtonModule,
+    ToolbarModule,
+    ToastModule,
+    CalendarModule,
+    DropdownModule,
+    ColorPickerModule
   ],
-  providers: [],
+  providers: [
+    UserService, MessageService, ConfirmationService, ValidatorService, FormBuilder
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
